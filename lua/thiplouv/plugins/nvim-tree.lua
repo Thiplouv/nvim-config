30153bcee39f6a9fb5872a9d9c36f9081af1bdfb
Thibault Plouviez <tplouviez@bwi.earth>
2023-11-15 17:16:06 +0100

    🔧 Plugins - Updates Nvim Tree Config

--- lua/thiplouv/plugins/nvim-tree.lua
+++ lua/thiplouv/plugins/nvim-tree.lua
@@ -1,17 +1,22 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    -- disable netrw at the very start of your init.lua
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    
+    config = function()
+    
+        -- disable netrw at the very start of your init.lua
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
-    require("nvim-tree").setup {
+    
+    require("nvim-tree").setup ({
         view = {
             width = 30,
+            relativenumber = false,
         },
         -- change folder arrow icons
         renderer = {
@@ -23,10 +28,15 @@ return {
                     folder = {
                         arrow_closed = "", -- arrow when folder is closed
                         arrow_open = "", -- arrow when folder is open
-                        },
                     },
                 },
             },
-        }
-  end,
+        },
+    })
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+    
+    keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+
+    end,
 }
