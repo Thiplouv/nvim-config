11bd6c81095bc971848b009eeea39396fe15e868
Thibault Plouviez <thibault.plouviez@icloud.com>
2024-09-27 17:33:33 +0200

    ✨ Plugins - Refines LSP and Treesitter config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,19 +3,39 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       autoformat = false,
-    },
-    -- servers = {
-    --   bashls = {},
-    yamlls = {
-      settings = {
-        yaml = {
-          keyOrdering = false,
+      servers = {
+        bashls = {},
+        clangd = {},
+        cmake = {},
+        omnisharp = {},
+        cssls = {},
+        dockerls = {},
+        gopls = {},
+        html = {},
+        jdtls = {},
+        tsserver = {},
+        jsonls = {},
+        texlab = {},
+        lua_ls = {},
+        rnix = {},
+        intelephense = {},
+        powershell_es = {},
+        pyright = {},
+        metals = {},
+        sqlls = {},
+        taplo = {},
+        terraformls = {},
+        vimls = {},
+        lemminx = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
         },
+        zls = {},
       },
     },
-    --   marksman = {},
-    --   terraformls = {},
-    --   helm_ls = {},
-    -- },
   },
 }
