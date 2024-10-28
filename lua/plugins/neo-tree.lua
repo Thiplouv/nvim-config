5297198b857df342c094ac4b1f84566b9d760a13
Thibault Plouviez <tplouviez@bwi.earth>
2024-10-28 17:37:21 +0100

    ⚡️ Neo-Tree - Tunes hidden files settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,19 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          always_show = { -- remains visible even if other settings would normally hide it
+            ".gitignore",
+          },
+          never_show = {
+            ".DS_Store",
+            ".git",
+          },
+        },
+      },
       window = {
         width = 30,
       },
