0436b92f0fad68224257c1deb0e1262a39fc489e
Thibault PLOUVIEZ <thibault.plouviez@epita.fr>
2025-01-06 22:28:17 +0100

    ✨ Lualine - Customized sections and separators to my tastes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,56 @@
+local icons = LazyVim.config.icons
+
 return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+
         globalstatus = false,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path() },
+        },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_z = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
       },
     },
   },
