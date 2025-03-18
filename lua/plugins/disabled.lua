b40c722cf74a0fdd8809dde94543baf1b6f279fd
Thibault Plouviez <thibault.plouviez@icloud.com>
2025-03-18 12:27:39 +0100

    🐛 Plugins - Ensures cmp is installed and enabled

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = true },
 }
